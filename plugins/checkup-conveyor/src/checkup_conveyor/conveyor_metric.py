067248a10a6a5a3789c3f5d24f4dfcd135b561d7
Gergely Soti <gergely.soti@dataminded.be>
2025-12-16 12:20:51 +0100

    finish conveyor metrics

--- plugins/checkup-conveyor/src/checkup_conveyor/conveyor_metric.py
+++ plugins/checkup-conveyor/src/checkup_conveyor/conveyor_metric.py
@@ -1,18 +1,48 @@
+from typing import ClassVar
+
 from checkup import Context
 from checkup_conveyor import ConveyorMetric
 
 import requests
 
 class ConveyorLastDeploymentTime(ConveyorMetric):
-    name: str = "Conveyor Last Deployment Time"
-    description: str = "Time of the last deployment in Conveyor"
-    unit: str = "timestamp"
+    name: ClassVar[str] = "Conveyor Last Deployment Time"
+    description: ClassVar[str] = "Time of the last deployment in Conveyor"
+    unit: ClassVar[str] = "timestamp"
+    diagnostic: str = 'Deploy the project again to update this value.'
 
     def calculate(self, context: Context, metrics: dict) -> None:
-        api_key = context['conveyor_api_key']
-        headers = {
-            "Authorization": f"Bearer {api_key}"
-        }
-        r = requests.get('https://app.conveyordata.com/api/v2/projects/42d0be19-7b39-46c3-ab49-c45f197f3a32/deployments',
-                     headers=headers).json()
+        proj_id = self.get_conveyor_project_id(context)
+        r = requests.get(f'{self.base_url}/projects/{proj_id}/deployments',
+                     headers=self.get_conveyor_api_headers(context)).json()
         self.value = r['deployment'][0]['deployedOn']
+
+
+class ConveyorIsDirtyDeployment(ConveyorMetric):
+    name: ClassVar[str] = "Conveyor Is Dirty Deployment"
+    description: ClassVar[str] = "True if the last deployment was dirty"
+    unit: ClassVar[str] = "boolean"
+    diagnostic: str = 'Commit changes to git, and deploy the project again.'
+
+    def calculate(
+        self, context: Context, metrics: dict
+    ) -> None:
+        proj_id = self.get_conveyor_project_id(context)
+        r = requests.get(f'{self.base_url}/projects/{proj_id}/builds',
+                     headers=self.get_conveyor_api_headers(context)).json()
+        self.value = r['builds'][0]['gitHash'].endswith('.dirty')
+
+
+class ConveyorLastRunStatus(ConveyorMetric):
+    name: ClassVar[str] = "Conveyor Last Run Status"
+    description: ClassVar[str] = "Status of the last run in Conveyor"
+    unit: ClassVar[str] = "string"
+
+
+    def calculate(self, context: Context, metrics: dict) -> None:
+        proj_id = self.get_conveyor_project_id(context)
+        env_id = self.get_environment_id(context)
+        r = requests.get(f'{self.base_url}/environments/{env_id}/application_runs',
+                         params={'sortedOn': 'SortedOn_None', 'sortedOrder': 'SortedOrder_None', 'projectId': proj_id},
+                     headers=self.get_conveyor_api_headers(context)).json()
+        self.value = r['applicationRuns'][0]['phase']
--- plugins/checkup-conveyor/test/test_conveyor_integration.py
+++ plugins/checkup-conveyor/test/test_conveyor_integration.py
@@ -1,7 +1,12 @@
 from checkup import CheckHub
-from checkup_conveyor.conveyor_metric import ConveyorLastDeploymentTime
+from checkup_conveyor import ConveyorProvider
+from checkup_conveyor.conveyor_metric import ConveyorLastDeploymentTime, ConveyorIsDirtyDeployment, ConveyorLastRunStatus
 
 
 def test_conveyor_integration():
-    r = CheckHub().with_metrics([ConveyorLastDeploymentTime]).measure()
+    r = CheckHub().with_metrics([
+        ConveyorLastDeploymentTime,
+        ConveyorIsDirtyDeployment,
+        ConveyorLastRunStatus,
+    ]).with_providers([[ConveyorProvider(project_name='activity-centers')]]).measure()
     assert r is not None
