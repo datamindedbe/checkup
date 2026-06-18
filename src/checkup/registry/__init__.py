3f6b36662f02bc1861458821230a8f1fe91cb100
CasperTeirlinck <casperteirlinck@gmail.com>
2026-06-18 15:03:40 +0200

    ...

--- src/checkup/registry/__init__.py
+++ src/checkup/registry/__init__.py
@@ -3,11 +3,13 @@ Plugin registry for discovering providers, metrics, and materializers.
 """
 
 from checkup.registry.discovery import (
+    Plugin,
     PluginRegistry,
     get_registry,
 )
 
 __all__ = [
+    "Plugin",
     "PluginRegistry",
     "get_registry",
 ]
